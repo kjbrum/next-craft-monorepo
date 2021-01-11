2f6f9b8202d22f2a1dcdccbf29bc681842b0fdbb
Kyle Brumm <kjbrumm@gmail.com>
2021-01-11 11:14:26 -0600

    Add toKebabCase helper

--- frontend/lib/helpers.js
+++ frontend/lib/helpers.js
@@ -178,3 +178,13 @@ export const parseVideoUrl = {
         return thumbUrl
     },
 }
+
+// Convert a string to kebabcase
+export const toKebabCase = (str = '') => {
+    return str
+        .match(
+            /[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g
+        )
+        .map(x => x.toLowerCase())
+        .join('-')
+}

f9569437c5ea3eb3743c7015e4da0e6d92d43da5
Kyle Brumm <kjbrumm@gmail.com>
2021-03-02 13:25:03 -0600

    Update postcss config

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,17 +1,26 @@
 module.exports = {
-    plugins: {
-        'postcss-import': {},
-        tailwindcss: {},
-        autoprefixer: {},
-        // Next.js defaults
-        'postcss-preset-env': {
-            autoprefixer: {
-                flexbox: 'no-2009',
-            },
-            stage: 3,
-            features: {
-                'custom-properties': false,
-            },
-        },
-    },
+    plugins:
+        process.env.NODE_ENV === 'production'
+            ? {
+                  'postcss-import': {},
+                  tailwindcss: {},
+                  autoprefixer: {},
+                  // Next.js defaults
+                  'postcss-flexbugs-fixes': {},
+                  'postcss-preset-env': {
+                      autoprefixer: {
+                          flexbox: 'no-2009',
+                          grid: 'autoplace',
+                      },
+                      stage: 3,
+                      features: {
+                          'custom-properties': false,
+                      },
+                  },
+              }
+            : {
+                  'postcss-import': {},
+                  tailwindcss: {},
+                  autoprefixer: {},
+              },
 }
